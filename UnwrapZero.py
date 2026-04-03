fa830ac00720906c57b4a4e4dc3724d99f466604
K <Akarfire@gmail.com>
2026-04-03 15:13:08 +0300

    [DEV] Parser implementation & bug fixes

--- UnwrapZero.py
+++ UnwrapZero.py
@@ -1,5 +1,5 @@
 # UTILITY
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 # Whter obj is iterable or not
 def is_iterable(obj):
@@ -13,14 +13,17 @@ def is_iterable(obj):
 # Logs some kind of message
 def log(message, type):
     print(type + " : ", message)
+    
+    if type == "ERROR":
+        raise Exception(message)
 
 
 # Contains a code template and all operations that need to be applied to it
 @dataclass
 class UnwrapTemplate:
-    out_file : str
-    template_code : str
-    operations : list[Operation]
+    out_file : str = ""
+    template_code : str = ""
+    operations : list[Operation] = field(default_factory=list)
     
 
 # OPERATIONS
@@ -39,7 +42,7 @@ class Operation:
 # Replaces all instances of "A" with every version of "B"
 class Replace(Operation):
     def __init__(self, arguments_):
-        super.__init__(arguments_)
+        super().__init__(arguments_)
         
     def execute(self, code : str) -> list[str]:
         
@@ -51,25 +54,120 @@ class Replace(Operation):
         find = self.arguments[0]
         for rep in range(1, len(self.arguments)):
             replacement = self.arguments[rep]
-            
-            if (is_iterable(replacement)):
+
+            if (type(replacement) != str and is_iterable(replacement)):
                 for i in replacement:
-                    out_branches.append(code.replace(find, i))
+                    out_branches.append(code.replace(find, str(i)))
                     
             else:
                 out_branches.append(code.replace(find, replacement))
+                
+        return out_branches
     
 
 # PARSING
 
+# If c is a separator sybmol
+def is_separator(c : str) -> bool:
+    return c in " :;,|-/\n"
+    
+
 # Parses provided code to determine required operations and template
 def Parse(code : str) -> list[UnwrapTemplate]:
     
+    result : list[UnwrapTemplate] = []
+    
+    currentTepmplate = UnwrapTemplate()
+    
     # Current sate
+    is_command : bool = False
+    is_command_name : bool = False
+    is_literal : bool = False
+    is_pyargument : bool = False
+    is_template : bool = False
+    is_special : bool = False
+    
+    command_name = ""
+    current_arguments = []
     # ...
     
     # Current token
     current_token : str = ""
+    
+    # Parsing loop
+    for c in code:
+        
+        if c == '\\': is_special = True
+        
+        if c == '@' and not is_special and not is_literal and not is_pyargument: 
+            
+            is_command = not is_command
+            if (is_command):
+                is_command_name = True
+                
+            elif is_template:
+                # print(command_name)
+                # print(current_token)
+                currentTepmplate.template_code = current_token
+                is_template = False
+                
+                # Appending result
+                result.append(currentTepmplate)
+                
+                current_token = ""
+                
+            else:
+                current_token = ""
+                # Command words resolving
+                
+                if command_name == "UWZ": current_arguments = [] 
+                
+                elif command_name == "File":
+                    currentTepmplate.out_file = current_arguments[0]
+                    current_arguments = []
+                    
+                elif command_name == "Reset":
+                    currentTepmplate = UnwrapTemplate()
+                    current_arguments = []
+                    
+                # OPERATION CREATION
+                # @Replace ... @ opertion
+                elif command_name == "Replace":
+                    currentTepmplate.operations.append(Replace(current_arguments))
+                    current_arguments = []
+                    
+                else:
+                    current_arguments = []
+                                    
+                
+        elif c == '`' and not is_special and not is_pyargument:
+            is_literal = not is_literal
+            if not is_literal:
+                current_arguments.append(current_token)
+                current_token = ""
+                
+        elif c == '%' and not is_special and not is_literal:
+            is_pyargument = not is_pyargument
+            if not is_pyargument:
+                pyarg = eval(current_token)
+                current_arguments.append(pyarg)
+                current_token = ""
+        
+        elif is_separator(c) and not (is_literal or is_pyargument or is_template) and not is_special:
+            if is_command_name:
+                is_command_name = False
+                command_name = current_token
+                current_token = ""
+                
+                if command_name == "Template":
+                    is_template = True
+                    current_token += c
+        
+        else:
+            current_token += c
+            is_special = False
+        
+    return result
        
 
 # UNWRAPPING
@@ -100,7 +198,11 @@ def Pack(unwrapped : list[list[str]], templates : list[UnwrapTemplate]) -> dict[
     output : dict[str : str] = {}
     
     for i, temp in enumerate(templates):
-        output[temp.out_file] = unwrapped[i]
+        compilation = ""
+        for s in unwrapped[i]:
+            compilation += s
+        
+        output[temp.out_file] = compilation
         
     return output
 
