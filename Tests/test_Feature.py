9e12021fb2689345869d7ea2df0d54a0790bc56b
K <Akarfire@gmail.com>
2026-04-04 02:14:14 +0300

    [TEST] Table command tested

--- Tests/test_Feature.py
+++ Tests/test_Feature.py
@@ -186,3 +186,139 @@ def test_multiple_replace_operations():
         print(expected)
         
     assert correct
+    
+    
+def test_table_string_arguments():
+    
+    input = '''
+    @UWZ 1.0 @
+    @File `file.py`@
+    @Table
+    `Param_1` | `Param_2` | `Param_3`
+          `1` |       `2` |       `3`
+          `0` |       `1` |       `2`
+    @
+    @Template
+    def FuncName_Index():
+        a = Param_4 * Param_5
+        return Param_1 + Param_2 + Param_3 + a
+    @
+    '''
+    
+    expected = {"file.py" : '''
+    def FuncName_Index():
+        a = Param_4 * Param_5
+        return 1 + 2 + 3 + a
+    
+    def FuncName_Index():
+        a = Param_4 * Param_5
+        return 0 + 1 + 2 + a
+    '''}
+    
+    result = UnwrapZero.Process(input)
+    
+    correct = result == expected
+    
+    if not correct:
+        print("Result: ")
+        print(result)
+        print("Expected: ")
+        print(expected)
+        
+    assert correct
+    
+    
+def test_table_string_and_one_iterable_arguments():
+    
+    input = '''
+    @UWZ 1.0 @
+    @File `file.py`@
+    @Table
+    `Param_1` | `Param_2` | `Param_3`
+          `1` |       `2` |  %["cool", "iterable"]%
+          `0` |       `1` |       `2`
+    @
+    @Template
+    def FuncName_Index():
+        a = Param_4 * Param_5
+        return Param_1 + Param_2 + Param_3 + a
+    @
+    '''
+    
+    expected = {"file.py" : '''
+    def FuncName_Index():
+        a = Param_4 * Param_5
+        return 1 + 2 + cool + a
+    
+    def FuncName_Index():
+        a = Param_4 * Param_5
+        return 1 + 2 + iterable + a
+    
+    def FuncName_Index():
+        a = Param_4 * Param_5
+        return 0 + 1 + 2 + a
+    '''}
+    
+    result = UnwrapZero.Process(input)
+    
+    correct = result == expected
+    
+    if not correct:
+        print("Result: ")
+        print(result)
+        print("Expected: ")
+        print(expected)
+        
+    assert correct
+    
+    
+def test_table_string_and_two_iterable_arguments():
+    
+    input = '''
+    @UWZ 1.0 @
+    @File `file.py`@
+    @Table
+    `Param_1` | `Param_2` | `Param_3`
+          `1` |  %[8, 6]% | %["cool", "iterable"]%
+          `0` |       `1` |       `2`
+    @
+    @Template
+    def FuncName_Index():
+        a = Param_4 * Param_5
+        return Param_1 + Param_2 + Param_3 + a
+    @
+    '''
+    
+    expected = {"file.py" : '''
+    def FuncName_Index():
+        a = Param_4 * Param_5
+        return 1 + 8 + cool + a
+    
+    def FuncName_Index():
+        a = Param_4 * Param_5
+        return 1 + 6 + cool + a
+    
+    def FuncName_Index():
+        a = Param_4 * Param_5
+        return 1 + 8 + iterable + a
+    
+    def FuncName_Index():
+        a = Param_4 * Param_5
+        return 1 + 6 + iterable + a
+    
+    def FuncName_Index():
+        a = Param_4 * Param_5
+        return 0 + 1 + 2 + a
+    '''}
+    
+    result = UnwrapZero.Process(input)
+    
+    correct = result == expected
+    
+    if not correct:
+        print("Result: ")
+        print(result)
+        print("Expected: ")
+        print(expected)
+        
+    assert correct
