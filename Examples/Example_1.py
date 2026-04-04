c0a7a98c35391cb6bd2f537e63083a15445c986f
K <Akarfire@gmail.com>
2026-04-04 13:47:08 +0300

    [EXAMPLE] Example with uwz syntax in comments

---
+++ Examples/Example_1.py
@@ -0,0 +1,17 @@
+#@UWZ 1.0 @
+#@File `./Example_1_unwrap.py` @
+
+#@Replace `FuncName` : `Func_1` , `Func_2` , `Func_3` @
+#@Replace `Index` : % range(10) % @
+
+#@Table
+#`Param_1` | `Param_2` | `Param_3`
+#      `1` |       `2` |       `3`
+#      `0` |       `1` |       `2`
+#@
+
+#@Template
+def FuncName_Index():
+    a = 3 * 4
+    return Param_1 + Param_2 + Param_3 + a
+#@
